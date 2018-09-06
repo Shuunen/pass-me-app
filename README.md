14ddd705c2bdf467160e0370e5a7c3b52bb6c0eb
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-09-06 16:52:51 +0200

    init vuido

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pass-me-app
-Pass me desktop app
+
+> Pass me desktop app
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn install
+
+# build
+yarn build
+
+# run the bundled script
+yarn start
+```
+
+For detailed explanation on how things work, consult the [Vuido documentation](https://vuido.mimec.org/).
---
+++ src/MainWindow.vue
@@ -0,0 +1,17 @@
+<template>
+  <Window title="pass-me-app" width="400" height="100" margined v-on:close="exit">
+    <Box>
+      <Text>Welcome to your Vuido application dudo :p</Text>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      this.$exit();
+    }
+  }
+}
+</script>
